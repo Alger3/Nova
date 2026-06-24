96ccc1f695140dbba99de2fd752a8f8848182db9
Guanjie Liang <guanjieliang3@gmail.com>
2026-06-24 23:09:47 +0800

    把子MCP服务都合并到一个主服务上

--- mcp-server/server.py
+++ mcp-server/server.py
@@ -1,9 +1,15 @@
 from fastmcp import FastMCP
 from config import load_config
+from tools import file_mcp, data_mcp, network_mcp, system_mcp
 
 
 config = load_config()
 mcp = FastMCP(config.server.name)
+
+mcp.mount(file_mcp)
+mcp.mount(data_mcp)
+mcp.mount(network_mcp)
+mcp.mount(system_mcp)
 
 if __name__ == "__main__":
     mcp.run(transport="http", 
